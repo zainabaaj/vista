d49fad85c26448efb1b2132bfec6be0267360ea7
Zeyna Baaj <zainabaaj@gmail.com>
2024-12-04 22:53:41 +0300

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+       navy: '#242b33'
+
       },
     },
   },
